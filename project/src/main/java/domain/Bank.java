f6c5d3ba742dae3e9ee19941a938061928e99a52
Nicolas Comet <nicolas.comet@gmail.com>
2017-09-12 20:30:17 +0200

    changed public signature

--- project/src/main/java/domain/Bank.java
+++ project/src/main/java/domain/Bank.java
@@ -12,7 +12,7 @@ class Bank {
     private static final Double bankAccountTotal = 9215.32;
 
     @Lock(key = "4b26e049-14f8-4f40-b742-aab87f0f560f")
-    Double bankAccountTotal() {
+    public Double bankAccountTotal() {
         return bankAccountTotal;
     }
 
